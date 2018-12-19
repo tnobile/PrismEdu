8cbb5461958aaff41d7232116093248e61925648
tnobile <takao.nosaka@gmail.com>
2018-12-19 21:15:02 +0100

    cleanup

--- 04.MVVMBasic/MVVMBasicApp.Module/MVVMBasicModule.cs
+++ 04.MVVMBasic/MVVMBasicApp.Module/MVVMBasicModule.cs
@@ -2,11 +2,7 @@
 using MVVMBasicApp.Module.Views;
 using Prism.Modularity;
 using Prism.Regions;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVMBasicApp.Module
 {
--- 05.InteractionRequest/InteractionRequestSampleApp/Bootstrapper.cs
+++ 05.InteractionRequest/InteractionRequestSampleApp/Bootstrapper.cs
@@ -1,11 +1,6 @@
 ﻿using InteractionRequestSampleApp.Views;
 using Prism.Unity;
 using Microsoft.Practices.Unity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Prism.Modularity;
 using InteractionRequestSampleModule;
